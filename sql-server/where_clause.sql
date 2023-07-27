960f68c6ec9b587623051855df1c9410272b113b
Carolina <carolsalgadoguadanhini@gmail.com>
2023-07-27 20:06:23 -0300

    add where clause file

---
+++ sql-server/where_clause.sql
@@ -0,0 +1,10 @@
+SELECT * FROM tbl_books
+
+SELECT * FROM tbl_books
+WHERE Book_Price <= 20
+
+SELECT Book_Name FROM tbl_books
+WHERE Book_Price > 20
+
+SELECT Book_Id, Book_Name FROM tbl_books
+WHERE Book_Name = 'Les Miserables'
